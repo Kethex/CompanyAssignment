76d1dcc8f94a24525aa365ee7336eb4c6b1875b8
ridwan.axmed@gmail.com <ridwan.axmed@gmail.com>
2022-11-21 22:37:02 +0000

    rudimentary typescript conversion

--- src/components/MainApplication.vue
+++ src/components/MainApplication.vue
@@ -40,7 +40,7 @@
     </div>
 </template>
 
-<script>
+<script lang="ts">
 
 import BikeList from './BikeList.vue'
 
--- src/services/CityBikes.ts
+++ src/services/CityBikes.ts
@@ -1,7 +1,8 @@
 export async function getBikes() {
 
     const response = await fetch('http://api.citybik.es/v2/networks/santander-cycles');
-    let res = response.json()
+    const res = response.json()
     return res
+    
 }
 
